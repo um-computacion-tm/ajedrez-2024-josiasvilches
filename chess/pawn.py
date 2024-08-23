eb24c945bbc0a2cde96c4a8709cda1cfca4d153b
josiasvilches <jl.vilches@alumno.um.edu.ar>
2024-08-23 18:01:47 -0300

    pawn new

--- chess/pawn.py
+++ chess/pawn.py
@@ -14,3 +14,21 @@ class Pawn(Piece):
         if start_columna == end_columna:
             if (end_fila - start_fila) == direction:
                 return True
+        if start_columna == end_columna:
+            if (end_fila - start_fila) == direction:
+                return True
+            # Movimiento inicial de dos pasos hacia adelante
+            if not self.__has_moved__ and (end_fila - start_fila) == 2 * direction:
+                return True
+        
+        # Movimiento de captura diagonal
+        if abs(start_columna - end_columna) == 1 and (end_fila - start_fila) == direction:
+            if board[end_fila][end_columna] is not None and board[end_fila][end_columna].get_color() != self.get_color():
+                return True
+    
+
+            return False
+
+    def move(self, end_fila, end_columna):
+        self.__has_moved__ = True
+        self.set_position(end_fila, end_columna)
