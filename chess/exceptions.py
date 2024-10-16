714255b55717a0c6d88f06dfb8b1fc5a965affa3
josiasvilches <jl.vilches@alumno.um.edu.ar>
2024-10-15 21:16:07 -0300

    codeclimate correction

--- chess/exceptions.py
+++ chess/exceptions.py
@@ -1,15 +1,15 @@
 class ChessError(Exception):
     """Base class for other chess-related exceptions"""
-    pass
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
 class InvalidMoveError(ChessError):
     """Raised when a move is invalid"""
     def __init__(self, message="Realizaste un movimiento inválido"):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
 
 class PieceNotFoundError(ChessError):
     """Raised when a piece is not found on the board"""
     def __init__(self, message="La pieza no fue encontrada en el tablero"):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
